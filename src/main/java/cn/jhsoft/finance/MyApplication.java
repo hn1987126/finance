34b2cf1465dd4ed29567a325053b6f2c86ad4e7a
chenyi <chenyi9@jd.com>
2017-08-24 16:31:04 +0800

    去除不需要的模块

--- src/main/java/cn/jhsoft/finance/MyApplication.java
+++ src/main/java/cn/jhsoft/finance/MyApplication.java
@@ -7,14 +7,14 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class JhsoftApplication extends SpringBootServletInitializer {
+public class MyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JhsoftApplication.class, args);
+		SpringApplication.run(MyApplication.class, args);
 	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(JhsoftApplication.class);
+		return application.sources(MyApplication.class);
 	}
 }
