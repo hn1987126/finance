079a8f429e167ee7f401014aa6cbc07d673817a7
chenyi <chenyi9@jd.com>
2017-09-01 13:08:39 +0800

    test

--- src/test/java/cn/jhsoft/finance/test/MyApplicationTests.java
+++ src/test/java/cn/jhsoft/finance/test/MyApplicationTests.java
@@ -1,16 +1,27 @@
 package cn.jhsoft.finance.test;
 
+import cn.jhsoft.finance.MyApplication;
+import cn.jhsoft.finance.common.utils.RedisUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MyApplication.class)
+@WebAppConfiguration
 public class MyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @Test
+    public void contextLoads() {
+        redisUtils.set("ght", "gonghaitao");
+        System.out.println(redisUtils.get("ght"));
+    }
 
 }
