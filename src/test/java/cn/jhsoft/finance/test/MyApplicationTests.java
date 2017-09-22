8f2040de7f9cb1bfe28817c8a6fef1e2ddde6c3b
chenyi <chenyi9@jd.com>
2017-09-22 11:43:03 +0800

    test

--- src/test/java/cn/jhsoft/finance/test/MyApplicationTests.java
+++ src/test/java/cn/jhsoft/finance/test/MyApplicationTests.java
@@ -10,18 +10,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MyApplication.class)
-@WebAppConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest(classes = MyApplication.class)
+//@WebAppConfiguration
 public class MyApplicationTests {
 
-    @Autowired
-    private RedisUtils redisUtils;
-
-    @Test
-    public void contextLoads() {
-        redisUtils.set("ght", "gonghaitao");
-        System.out.println(redisUtils.get("ght"));
-    }
+//    @Autowired
+//    private RedisUtils redisUtils;
+//
+//    @Test
+//    public void contextLoads() {
+//        redisUtils.set("ght", "gonghaitao");
+//        System.out.println(redisUtils.get("ght"));
+//    }
 
 }
